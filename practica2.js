d5f4182a744b1d5cc38c1b750bb19096670eac59
srojgam0907 <srojgam0907@g.educaand.es>
2026-01-13 19:22:38 +0100

    js version 2: funciones y eventos terminados

--- practica2.js
+++ practica2.js
@@ -1,29 +1,56 @@
+let numero= 0;
+
 const contador= document.querySelector("#contador");
-const numero= document.querySelector("#numero");
+const mensajeError= document.querySelector("#error");
+
+const aumentar= document.querySelector("#aumentar");
+const disminuir= document.querySelector("#disminuir");
+const reiniciar= document.querySelector("#reiniciar");
+
+//FUNCIONES
+
+//Para actualizar el valor en la pantalla
+function actualizar() {
+    contador.textContent= numero;
+}
+
+//Para mostrar el mensaje de error durante un tiempo determinado
+function mostrarError() {
+    mensajeError.classList.remove("oculto");
+
+    setTimeout(function() {
+        mensajeError.classList.add("oculto");
+    }, 1000);
+}
+
+//EVENTOS
 
 //Evento del boton aumentar
-contador.addEventListener("aumentar", function() {
+aumentar.addEventListener("click", function() {
+    numero++;
+    actualizar();
 
     if(numero > 10) {
-        alert("No se puede de 10");
+        alert("No se puede superar el 10");
     }
-
 
 });
 
 //Evento del boton Disminuir
-contador.addEventListener("disminuir", function() {
-
-    if(numero < 0) {
-        
+disminuir.addEventListener("click", function() {
+    if(numero == 0) {
+        mostrarError();
+        return;
     }
-
+    
+    numero--;
+    actualizar();
 
 });
 
 //Evento del boton reiniciar
-contador.addEventListener("reiniciar", function() {
-
-
+reiniciar.addEventListener("click", function() {
+    numero= 0;
+    actualizar();
 
 });
